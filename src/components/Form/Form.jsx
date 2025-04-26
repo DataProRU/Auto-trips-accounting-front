6f5ddc2f44a9296b8573c3032e098443c471fbf5
Emirlan Dogdurbaev <jigit38@gmail.com>
2025-04-26 17:29:01 +0600

    [ITO-207] customize project

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,12 @@
+const Form = () => {
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" />
+      </form>
+    </div>
+  );
+};
+
+export default Form;
---
+++ src/pages/ReportPage/ReportPage.jsx
@@ -0,0 +1,14 @@
+import Form from "../../components/Form/Form";
+import Header from "../../components/Header/Header";
+
+const ReportPage = () => {
+  return (
+    <div>
+      <Header />
+
+      <Form />
+    </div>
+  );
+};
+
+export default ReportPage;
