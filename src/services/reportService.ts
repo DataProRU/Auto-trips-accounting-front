60e80a61676a020ce8bae775c166d16f182ba8f1
Emirlan Dogdurbaev <jigit38@gmail.com>
2025-05-25 22:03:35 +0600

    [ITO-208] fix urls

--- src/services/reportService.ts
+++ src/services/reportService.ts
@@ -12,7 +12,7 @@ export const fetchInitialData = createAsyncThunk<
   { rejectValue: string }
 >("report/fetchInitialData", async (_, { rejectWithValue }) => {
   try {
-    const response = await $api.get<IInitialDataResponse>("get_form_data");
+    const response = await $api.get<IInitialDataResponse>("/get_form_data");
     const { data } = response;
     return {
       ...data,
